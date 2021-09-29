9e26c634c037df25dd0a8c4f18008b06ba35639a
Anas Ansari <anas_ansar1@outlook.com>
2021-09-29 15:18:05 +0100

    Small Change

--- dist/index.js
+++ dist/index.js
@@ -25,7 +25,7 @@ app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_parser_1.default)());
 app.use(body_parser_1.default.json());
 app.use((0, cors_1.default)({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
 })); // Enables the OPTIONS request check in our API
 /* SETUP ROUTES */
